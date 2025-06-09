83cc609c8fd429bbb31e1c14198b864935010508
ymka3646 <yamakia@cy-com.co.jp>
2025-06-09 16:53:15 +0900

    ヌメラ

--- src/main/java/com/example/music_management/controller/LoginController.java
+++ src/main/java/com/example/music_management/controller/LoginController.java
@@ -2,7 +2,7 @@ package com.example.music_management.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+//ヌメラ
 @Controller
 public class LoginController {
     
